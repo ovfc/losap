6b22c960d7ba3af3cee063da5eca4409443b6aeb
Bryce <swarvingprogrammer@gmail.com>
2021-02-27 20:46:04 +0000

    Fills out migration.

--- db/migrate/20210216022856_add_collateralduty.rb
+++ db/migrate/20210216022856_add_collateralduty.rb
@@ -1,7 +1,19 @@
 class AddCollateralduty < ActiveRecord::Migration
-  def up
+  def self.up
+    create_table :collateraldutys do |t|
+      t.datetime :start_time
+      t.datetime :end_time
+      t.integer :member_id
+
+      t.timestamps
+    end
+    add_column :collateraldutys, :deleted, :boolean, :default => false, :null => false
+    add_index :collateraldutys, :member_id
   end
 
-  def down
+  def self.down
+    drop_table :collateraldutys
+    remove_column :collateraldutys, :deleted
+    remove_index :collateraldutys, :member_id
   end
 end
